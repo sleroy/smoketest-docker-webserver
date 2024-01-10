8170d9dbd503dcdcdc539142c832af19e7e8a4cd
Sylvain Leroy <sleroy0@gmail.com>
2024-01-10 10:47:31 +0100

    Tests

---
+++ __tests__/utils.test.js
@@ -0,0 +1,10 @@
+const os = require("os");
+const utils = require("../lib/utils");
+
+test("validates that getHostName returns the os.hostname", () => {
+  expect(utils.getHostname()).toBe(os.hostname());
+});
+
+test("validates that we collect network interfaces", () => {
+  expect(utils.getHostname()).toBe(os.hostname());
+});
