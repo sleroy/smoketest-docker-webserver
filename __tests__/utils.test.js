551269b313075f94a138026eb267fe16f6555aff
Sylvain Leroy <sleroy0@gmail.com>
2024-01-10 11:01:41 +0100

    Upgrading test

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -5,4 +5,11 @@ test("validates that getHostName returns the os.hostname", () => {
   expect(utils.getHostname()).toBe(os.hostname());
 });
 
-test("validates that we collect network interfaces", () => {});
+test("validates that we collect network interfaces", () => {
+  const networkInterfaces = utils.getServerIP();
+  expect(networkInterfaces).toBeDefined();
+  for (let key in networkInterfaces) {
+    expect(networkInterfaces[key]).toBeDefined();
+    expect(networkInterfaces[key].length).toBeGreaterThan(0);
+  }
+});
